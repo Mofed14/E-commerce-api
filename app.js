dc5ec21b4ec58b1d8aa59303c0cb0b1b69ee2d6a
mofed <mofed.salah.hana@gmail.com>
2023-04-07 19:59:46 +0200

    uploads

--- app.js
+++ app.js
@@ -54,25 +54,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static("./public"));
 app.use(fileUpload({ useTempFiles: true }));
-
-app.get("/", (req, res) => {
-  // console.log(req.cookies);
-
-  res.send("Hello Ecommerce");
-});
-
-app.get("/api/v1", (req, res) => {
-  // console.log(req.cookies);
-  // Beacuse signed: true I found the cookie in signedCookies
-  console.log(req.signedCookies);
-  res.send("Hello Ecommerce");
-});
 
 // 2- then Express tries to hit all the routes. and if it doesn't find the root,
 app.use("/api/v1/auth", authRouter);
